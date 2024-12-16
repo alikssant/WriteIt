319ddda2c0a4eacc21a2249c4d149bdbd185ead8
Alex Abatov <a.aliksant315@gmail.com>
2024-12-16 18:53:24 +0900

    Header component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "components/Header/Header";
 
 export function App() {
   return (
     <div>
-      React Redux Starter :-)
+      <Header></Header>
       <Outlet />
     </div>
   );
