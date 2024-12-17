c3162350926f68bdcba7a09ac552471b1c413871
Alex Abatov <a.aliksant315@gmail.com>
2024-12-18 06:43:57 +0900

    Notelist created

--- src/pages/NoteBrowser/NoteBrowser.jsx
+++ src/pages/NoteBrowser/NoteBrowser.jsx
@@ -1,15 +1,10 @@
 import { TextCard } from "components/TextCard/TextCard";
+import { NoteList } from "containersNoteList/NoteList";
 
 export function NoteBrowse(props) {
   return (
     <>
-      <TextCard
-        title={"Super note"}
-        subtitle={"10.12.2024"}
-        content={"An apple a day keeps the doctor away"}
-        onClick={() => alert("OnClick !")}
-        onClickTrash={() => alert("OnclickTrash !")}
-      />
+      <NoteList />
     </>
   );
 }
