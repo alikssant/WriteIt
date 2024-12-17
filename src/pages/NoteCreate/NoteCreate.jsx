b49cd651f151e627e18b957cddef08a4157650fb
Alex Abatov <a.aliksant315@gmail.com>
2024-12-18 07:09:42 +0900

    NoteForm created

--- src/pages/NoteCreate/NoteCreate.jsx
+++ src/pages/NoteCreate/NoteCreate.jsx
@@ -1,3 +1,8 @@
+import { NoteForm } from "components/NoteForm/NoteForm";
 export function NoteCreate(props) {
-  return <>NoteCreate</>;
+  return (
+    <>
+      <NoteForm title={"New Note"} />
+    </>
+  );
 }
