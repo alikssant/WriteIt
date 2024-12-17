990cd704ea6f235ced3253c64479acf346f0dc6a
Alex Abatov <a.aliksant315@gmail.com>
2024-12-18 04:15:55 +0900

    TextCard component created

--- src/pages/NoteBrowser/NoteBrowser.jsx
+++ src/pages/NoteBrowser/NoteBrowser.jsx
@@ -1,3 +1,15 @@
+import { TextCard } from "components/TextCard/TextCard";
+
 export function NoteBrowse(props) {
-  return <>NoteBrowse</>;
+  return (
+    <>
+      <TextCard
+        title={"Super note"}
+        subtitle={"10.12.2024"}
+        content={"An apple a day keeps the doctor away"}
+        onClick={() => alert("OnClick !")}
+        onClickTrash={() => alert("OnclickTrash !")}
+      />
+    </>
+  );
 }
