083403bebaba0616b7b679fca1be626a75bf847a
Alex Abatov <a.aliksant315@gmail.com>
2024-12-24 06:02:55 +0900

    User profile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,36 @@ import s from "./style.module.css";
 import logoSrc from "assets/images/logo.png";
 import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
 import { useNavigate } from "react-router-dom";
+import { AuthAPI } from "api/auth";
+import { setUser } from "store/auth/auth-slice";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 export function Header(props) {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.authSlice.auth.user);
+
+  const signout = () => {
+    AuthAPI.signout();
+    dispatch(setUser(null));
+  };
+
+  const renderAuthProfil = () => {
+    return (
+      <div>
+        <img
+          src={`https://api.dicebear.com/5.x/bottts/svg?seed=${user.email}`}
+          style={{ width: 40 }}
+          className="rounded-circle"
+        />
+        <div>Welcome, {user.email} </div>
+        <Link to="#" onClick={signout}>
+          Signout
+        </Link>
+      </div>
+    );
+  };
   return (
     <div className={`row ${s.container}`}>
       <div className="col-xs-12 col-sm-4">
@@ -15,11 +43,7 @@ export function Header(props) {
           image={logoSrc}
         />
       </div>
-      <div className="col-xs-12 col-sm-8 text-end">
-        <ButtonPrimary onClick={() => navigate("/note/new")}>
-          Add note +{" "}
-        </ButtonPrimary>
-      </div>
+      <div className="col-xs-12 col-sm-8 text-end"> {renderAuthProfil()} </div>
     </div>
   );
 }
