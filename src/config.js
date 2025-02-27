fc6b225904dedb12298cd6ebc1cb4fa1d1d7646b
Abatov  O <98354444+alikssant@users.noreply.github.com>
2025-02-28 08:44:34 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id",
+   apiKey: "AIzaSyBVCbOJVIhJvlW_skG-5a2m8GlG27qSjWc",
+  authDomain: "writeit-app.firebaseapp.com",
+  projectId: "writeit-app",
+  storageBucket: "writeit-app.firebasestorage.app",
+  messagingSenderId: "563606429954",
+  appId: "1:563606429954:web:d45695486e929ec009a21b",
 };
